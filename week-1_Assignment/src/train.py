80777d69cd8acf4bb6e09f0490a6a57df34e95c1
demo <abdullah.qureshi@emumba.com>
2026-06-18 15:07:44 +0500

    after ruff

--- week-1_Assignment/src/train.py
+++ week-1_Assignment/src/train.py
@@ -6,8 +6,12 @@ from evaluate import evaluate_model
 data = load_iris()
 X, y = data.data, data.target
 
+
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
+    X,
+    y,
+    test_size=0.2,
+    random_state=42,
 )
 
 model = RandomForestClassifier()
