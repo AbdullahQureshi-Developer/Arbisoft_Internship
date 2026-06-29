29244e61f7158e248b5a8acd4cad1f72e0f199ba
demo <abdullah.qureshi@emumba.com>
2026-06-29 16:53:47 +0500

    clean extra import

--- week-1_Assignment/src/__init__.py
+++ week-1_Assignment/src/__init__.py
@@ -1,4 +1,4 @@
-from .evaluate import evaluate_model as evaluate_model
-from .preprocess import clean_data as clean_data
+from .evaluate import evaluate_model
+from .preprocess import clean_data 
 
 __all__ = ["clean_data", "evaluate_model"]
